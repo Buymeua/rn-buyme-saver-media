8242517119df27303303764e7cc2b8c17eed51b7
Serhii Lavryniuk <lavrinyuk.serik@gmail.com>
2024-11-06 18:00:14 +0200

    added support video

--- ios/RnBuymeSaverMediaModule.swift
+++ ios/RnBuymeSaverMediaModule.swift
@@ -1,16 +1,12 @@
 import ExpoModulesCore
 import Photos
-import UIKit
-
-import ExpoModulesCore
-import Photos
-import UIKit
+import AVFoundation
 
 public class RnBuymeSaverMediaModule: Module {
   public func definition() -> ModuleDefinition {
     Name("RnBuymeSaverMedia")
 
-    Events("DownloadProgress") // Объявляем событие
+    Events("DownloadProgress")
 
     AsyncFunction("downloadFileToGallery") { (urlString: String) in
       guard let url = URL(string: urlString) else {
@@ -19,7 +15,7 @@ public class RnBuymeSaverMediaModule: Module {
 
       let downloader = FileDownloader()
       downloader.startDownload(from: url) { progress in
-        self.sendEvent("DownloadProgress", ["progress": progress]) // Отправляем событие прогресса
+        self.sendEvent("DownloadProgress", ["progress": progress])
       } completion: { fileURL, error in
         if let fileURL = fileURL {
           self.saveToGallery(fileURL: fileURL)
@@ -33,50 +29,37 @@ public class RnBuymeSaverMediaModule: Module {
   private func saveToGallery(fileURL: URL) {
     PHPhotoLibrary.requestAuthorization { status in
       if status == .authorized {
-        if let image = UIImage(contentsOfFile: fileURL.path) {
-          UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-          print("Image saved to gallery successfully.")
+        // Проверка на тип файла (видео или изображение)
+        let fileExtension = fileURL.pathExtension.lowercased()
+        if ["mp4", "mov", "webm"].contains(fileExtension) {
+          self.saveVideoToGallery(fileURL: fileURL)
         } else {
-          print("Failed to load image from URL for saving.")
+          self.saveImageToGallery(fileURL: fileURL)
         }
       } else {
         print("No permission to save to gallery.")
       }
     }
   }
-}
 
-class FileDownloader: NSObject, URLSessionDownloadDelegate {
-  private var onProgress: ((Double) -> Void)?
-  private var onComplete: ((URL?, Error?) -> Void)?
-
-  func startDownload(from url: URL, onProgress: @escaping (Double) -> Void, completion: @escaping (URL?, Error?) -> Void) {
-    self.onProgress = onProgress
-    self.onComplete = completion
-    let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-    let downloadTask = session.downloadTask(with: url)
-    downloadTask.resume()
+  private func saveImageToGallery(fileURL: URL) {
+    if let image = UIImage(contentsOfFile: fileURL.path) {
+      UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+      print("Image saved to gallery successfully.")
+    } else {
+      print("Failed to load image from URL for saving.")
+    }
   }
 
-  func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-    let progress = Double(totalBytesWritten) / Double(totalBytesExpectedToWrite) * 100
-    onProgress?(progress)
-  }
-
-  func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-    let fileManager = FileManager.default
-    let cachesDirectory = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)[0]
-    let cachedFileURL = cachesDirectory.appendingPathComponent(location.lastPathComponent)
-
-    do {
-      if fileManager.fileExists(atPath: cachedFileURL.path) {
-        try fileManager.removeItem(at: cachedFileURL)
+  private func saveVideoToGallery(fileURL: URL) {
+    PHPhotoLibrary.shared().performChanges({
+      PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: fileURL)
+    }) { success, error in
+      if success {
+        print("Video saved to gallery successfully.")
+      } else {
+        print("Failed to save video to gallery: \(String(describing: error))")
       }
-
-      try fileManager.moveItem(at: location, to: cachedFileURL)
-      onComplete?(cachedFileURL, nil)
-    } catch {
-      onComplete?(nil, error)
     }
   }
 }
